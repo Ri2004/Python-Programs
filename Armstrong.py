76d862be81c54bd5164fd8183f476b0ac58ec12f
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-04-30 14:25:19 +0530

    Modified

--- Armstrong.py
+++ Armstrong.py
@@ -1,11 +1,12 @@
 num = int(input("Enter A Number: "))
+num_length = len(str(num))
 temp = num
 dig = 0
 
 while num>0:
     digit = num%10
-    rem = digit**3
-    dig = dig+rem  
+    rem = digit**num_length
+    dig = dig+rem  # 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474, 54748,
     num = num//10
     
 if temp == dig:
