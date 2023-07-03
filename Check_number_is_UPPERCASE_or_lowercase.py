0882afa0c8d32af265b43b3a444206da35a35afa
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-03 18:39:24 +0530

    Modified

--- Check_number_is_UPPERCASE_or_lowercase.py
+++ Check_number_is_UPPERCASE_or_lowercase.py
@@ -1,6 +1,6 @@
 ch = input("Enter Alphabet: ")
 
-if ord(ch)>=65 and ord(ch)<=90:
+if ch.isupper():
     print("UPPERCASE")
-elif ord(ch)>=97 and ord(ch)<=122:
+elif ch.islower():
     print("lowercase")
