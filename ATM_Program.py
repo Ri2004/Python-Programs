cf6262c05d716664c304b219b332b0ca3d482264
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-10 18:48:42 +0530

    Modified

--- ATM_Program.py
+++ ATM_Program.py
@@ -2,11 +2,20 @@ a = int(input("Enter Initial Amount: "))
 choice = input("Choose\n c for credit\n d for debit\n b for balance\n")
 
 if choice == 'c':
-    c = int(input("Enter Credit Amount: "))
-    print(f"Credited Amount is {a+c}")
+    print(f"Yor Balance Amount is: {a}")
+    deposit = int(input("Enter Deposit Amount: "))
+    
+    if deposit>0:
+        print(f"Your Balance Amount is {a+deposit}")
 elif choice == 'd':
-    d = int(input("Enter Debit Amount: "))
-    print(f"Debited Amount is {a-d}")
+    print(f"Yor Balance Amount is: {a}")
+    withdraw = int(input("Enter Debit Amount: "))
+    
+    if withdraw<a:
+        print(f"Your Balance Amount is {a-withdraw}")
+    elif withdraw>a:
+        print("Nothing to withdraw")
+        
 elif choice == 'b':
     print(f"Balance Amount is {a}")
 else:
