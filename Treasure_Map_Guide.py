d814b06ca45d8b7bce048f537131845c3a25ab4a
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-29 16:02:24 +0530

    Modified

--- Treasure_Map_Guide.py
+++ Treasure_Map_Guide.py
@@ -7,6 +7,6 @@ position = input("Where do you want to put the treasure? ")
 
 column_number = int(position[0])
 row_number = int(position[1])
-map[row_number-1][column_number-1] = 'X'
+map[row_number-1][column_number-1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
