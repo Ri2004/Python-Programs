65d60dede856e49ec0ef20ce14389e2a78f54821
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-30 15:52:49 +0530

    Modified

--- Print_Minimum_Number.py
+++ Print_Minimum_Number.py
@@ -5,7 +5,7 @@ print(student_scores)
 
 lowest_score = 0
 for min_num in student_scores:
-    if min_num > lowest_score:
+    if min_num < lowest_score:
         lowest_score = min_num 
 
 print(f"The highest score in the class is: {lowest_score}")
