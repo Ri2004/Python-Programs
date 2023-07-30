1ec6a529bcd32b81e462c7803de4308411f9d8b0
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-07-30 16:09:34 +0530

    Modified

--- Print_Minimum_Number.py
+++ Print_Minimum_Number.py
@@ -3,9 +3,9 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-lowest_score = 0
-for min_num in student_scores:
-    if min_num < lowest_score:
-        lowest_score = min_num 
+lowest_score = student_scores[0]
+for min_num in range(1, len(student_scores)):
+    if lowest_score > student_scores[min_num]:
+        lowest_score = student_scores[min_num] 
 
-print(f"The highest score in the class is: {lowest_score}")
+print(f"The lowest score in the class is: {lowest_score}")
