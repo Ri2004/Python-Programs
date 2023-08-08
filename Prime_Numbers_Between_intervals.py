53e3727d74ff4d94c6afeae0733d28d8eb9db9cc
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-08 19:42:44 +0530

    New Commit

--- Prime_Numbers_Between_intervals.py
+++ Prime_Numbers_Between_intervals.py
@@ -1,13 +1,13 @@
-prime = True
 def check_prime(n):
+    prime = True
     if n==0 or n==1:
         prime = False
     else:
         for i in range(2,n):
             if n%i == 0:
-                return False
-            else:
-                return True
+                prime = False
+                break
+    return prime
                 
 a = int(input("Enter Low Limit: "))
 b = int(input("Enter Upper Limit: "))
