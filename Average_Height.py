0474e26cfb67a47fc23591ff4dff2986d9f3344b
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-09 20:05:22 +0530

    Modified

--- Average_Height.py
+++ Average_Height.py
@@ -1,11 +1,7 @@
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
 
-
-#Write your code below this row 👇
 # Important You should not use the sum() or len() functions in your answer. You should try to replicate their functionality using what you have learnt about for loops.
 
 sum_of_height = 0
