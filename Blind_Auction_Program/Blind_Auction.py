b8cc280b8a5811f883959a6575fdf627d322782b
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-11 21:38:58 +0530

    New Commit

---
+++ Blind_Auction_Program/Blind_Auction.py
@@ -0,0 +1,33 @@
+# from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+from art import logo
+print(logo)
+
+auction ={}
+end_bidder = False
+
+
+def find_highest_bid(bidding_record):
+  highest_bid = 0
+  winner = ""
+
+  for name in bidding_record:
+    amount = bidding_record[name]
+    if amount > highest_bid:
+      highest_bid = amount
+      winner = name
+
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+  
+while not end_bidder:
+  bidder_name = input("What is your name?: ")
+  bid_amount = int(input("What is your bid?: $"))
+
+  auction[bidder_name] = bid_amount
+  
+  any_other_person = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+  if any_other_person == "no":
+    end_bidder = True
+
+    find_highest_bid(auction)
+#   clear()
