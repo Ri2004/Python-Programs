e28bab4f7aa729b17107f784423b579b11ec5e5f
Rishabh Chatterjee <rishabhchatterjee738@gmail.com>
2023-08-24 15:30:50 +0530

    Modified

--- Searching_a_number_from_list_and_position.py
+++ Searching_a_number_from_list_and_position.py
@@ -2,11 +2,16 @@ def checkNum(lis, target):
     for i in range(len(lis)):
         if lis[i] == target:
             return i
-    
-    return -1
+        
+        elif lis[i] != target:
+            lis.insert(i, target)
+            lis.sort()
+            return lis.index(target)
 
 num = input().split()
 num = list(map(int, num))
+sorted_num = sorted(num)
+print(sorted_num)
 target = int(input("Enter Target Value: "))
-result = checkNum(num, target)
+result = checkNum(sorted_num, target)
 print(result)
